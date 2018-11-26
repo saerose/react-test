59111d3e29655802b157ebbcde2aaff8cba820ac
saerose <sbarcelocompte@gmail.com>
2018-11-26 20:49:24 +0100

    refactor: changed store to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import Provider from "react-redux/es/components/Provider";
+// import store from "./store";
+import { createStore } from 'redux';
+// import reducers from './redux/reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
 registerServiceWorker();
