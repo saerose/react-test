155e9ff9896b81e2910acdd8c3d400550ad2e20e
saerose <sbarcelocompte@gmail.com>
2018-11-26 20:54:50 +0100

    chore: starting basic ui in app.js to split later in diferent components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ export default class App extends Component {
     return (
         <div>
           <h1>Welcome to Colvin React Coding Challenge</h1>
-          <div></div>
+          <div>
+              <div><button>new card</button></div> <div>Get cards from api</div> <div>create json / pdf / other</div>
+            <div>
+                black deck list
+            </div>
+            <div>
+                white deck list
+            </div>
+          </div>
         </div>
     );
   }
