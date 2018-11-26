45b6ca90bd68833c107279e8a94fdf1037ef7fce
saerose <sbarcelocompte@gmail.com>
2018-11-26 22:38:37 +0100

    chore: new types for data fetching

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,9 @@
+// ===== cards.js =====
 export const ADD_CARD = 'ADD_CARD';
 export const EDIT_CARD = 'EDIT_CARD';
 export const REMOVE_CARD = 'REMOVE_CARD';
+
+// ===== data.js =====
+export const FETCH_DATA_PENDING = 'FETCH_DATA_PENDING';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_DATA_ERROR = 'FETCH_DATA_ERROR';
