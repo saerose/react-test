97f212a5346fc009b2e5dfcc1c2b1c7dbee95891
saerose <sbarcelocompte@gmail.com>
2018-11-28 22:06:33 +0100

    feat: created card actions component for basic button action

---
+++ src/components/CardActions/CardActions.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+export default class CardActions extends Component {
+    render() {
+        return (
+                <div>
+                    <button>new card</button>
+                    <div>Get cards from api</div>
+                    <div>create json / pdf / other</div>
+                </div>
+        )
+    }
+};
