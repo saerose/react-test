fc66c531e39db7dde331d7749bc4a10a5e07dd8f
saerose <sbarcelocompte@gmail.com>
2018-11-28 21:29:36 +0100

    feat: created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Title } from "./styles";
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Title>The biggest blackest box</Title>
+            </div>
+        )
+    }
+};
