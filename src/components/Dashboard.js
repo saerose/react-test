972e382ed3bf5ba0419ef7685321adb8a7eae3c0
saerose <sbarcelocompte@gmail.com>
2018-11-28 21:30:29 +0100

    chore: changed header for header component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>The biggest blackest box</h1>
+                <Header />
                 <div>
                     <div>
                         <button>new card</button>
