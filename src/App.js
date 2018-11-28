662d42899ddf0dca9886264531617c5b29dd958e
saerose <sbarcelocompte@gmail.com>
2018-11-28 21:39:47 +0100

    chore: added global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Dashboard from "./components/Dashboard";
+import { GlobalStyle } from "./styles";
 
 export default class App extends Component {
   render() {
     return (
-        <Dashboard />
+        <div>
+          <GlobalStyle/>
+          <Dashboard />
+        </div>
     );
   }
 }
