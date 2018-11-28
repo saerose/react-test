7f309b3f36b1277175d06b5091cb055fa69b9733
saerose <sbarcelocompte@gmail.com>
2018-11-28 22:06:11 +0100

    chore: modified global styles

--- src/styles.js
+++ src/styles.js
@@ -3,9 +3,8 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    padding: 0;
     width: 100%
     font-family: 'Nunito', sans-serif;
   }
-`
+`;
 
