2e0582fadf691635aa159ac064d97ca043ca4317
saerose <sbarcelocompte@gmail.com>
2018-11-29 20:16:10 +0100

    chore: added combined reducers to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,16 @@
-import { combineReducers } from "redux";
 
-const store = combineReducers({});
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import reducer from './reducers/cards.reducer';
+import api from './middleware/api';
+import config from './../config';
+// import { persistStore, /*persistReducer*/ } from 'redux-persist';
 
-export default store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const configureStore = () => {
+    const reducers = combineReducers({ apiReducer: reducer });
+    const store = createStore( reducers, composeEnhancers(applyMiddleware(api(config.baseUrl))));
+    return { store };
+};
+
+export default configureStore;
