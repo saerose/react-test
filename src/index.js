2505be79e810613e8a54b0d64b0be1e676b9b8a2
saerose <sbarcelocompte@gmail.com>
2018-11-29 20:30:19 +0100

    feat: correctly setting up redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import Provider from "react-redux/es/components/Provider";
-// import store from "./store";
-import { createStore } from 'redux';
+import configureStore from "./store/store";
+// import { createStore } from 'redux';
 // import reducers from './redux/reducers';
 
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__());
+// let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
