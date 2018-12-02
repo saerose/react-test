afec35905c111a4da88af0213488f9fa096cb4cf
saerose <sbarcelocompte@gmail.com>
2018-12-02 17:10:06 +0100

    chore: black and white cards with same background color using props

--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import connect from 'react-redux/es/connect/connect';
 import { Card } from './styles';
 
 export default class SingleCard extends Component {
 
   render() {
     return (
-      <Card>
-        {this.props.color}
+      <Card color={this.props.color}>
         {this.props.text}
       </Card>
     )
--- src/components/SingleCard/styles.js
+++ src/components/SingleCard/styles.js
@@ -1,11 +1,27 @@
 import styled from 'styled-components';
 
-const Card = styled.div`
-  display: flex;
-  width: 150px;
-  height:200px;
-  background-color: lavender;
-  margin: 10px;
-`;
+const opposite = {
+  black: 'white',
+  white: 'black'
+};
 
+const Card = styled.div`
+  width: 150px;
+  height: 200px;
+  background-color: ${props => props.color};
+  margin: 10px;
+  color: ${props => opposite[props.color]};
+  border: 1.4px solid #000000;
+  border-radius: 7px;
+  padding: 5px;
+  font-weight: 600;
+  // outline: none;
+  // border: 2px solid #A9B2C3;
+  // white-space: nowrap;
+  // box-shadow: inset 0 -4px 0 0 #A9B2C3;
+  // border-radius: 7px;
+  // font-size: 14px;
+  // padding: 14px 20px 18px;
+  // min-width: 100px;
+`;
 export { Card }
