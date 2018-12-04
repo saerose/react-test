a6a892cca95031453b0aad972d2a60615ca28f43
saerose <sbarcelocompte@gmail.com>
2018-12-04 19:27:17 +0100

    refactor: error at importing edit card action

--- src/components/EditCard/EditCard.js
+++ src/components/EditCard/EditCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {addCard} from '../../store/actions/cards.actions';
+import {editCard} from '../../store/actions/cards.actions';
 import connect from 'react-redux/es/connect/connect';
 import {
   PopUpWrapper,
