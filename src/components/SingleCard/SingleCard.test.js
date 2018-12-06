8f0251acd7003de048e617ecf468a1e66d661c5c
saerose <sbarcelocompte@gmail.com>
2018-12-06 22:37:51 +0100

    test: started testing, tested delete button, all tests passing

--- src/components/SingleCard/SingleCard.test.js
+++ src/components/SingleCard/SingleCard.test.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import SingleCard from './components/SingleCard/SingleCard';
-
-import configureStore from './store/store'
-
+import SingleCard from './SingleCard';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import { shallow } from 'enzyme';
 // import { stub } from 'sinon';
+
+import configureStore from '../../store/store'
 
 const store = configureStore()
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const component = shallow(<SingleCard store={store}/>)
-  console.log(component.html())
-});
+describe('Single Card', () => {
+  it('Should appear in DOM as <div>', () => {
+    const component = shallow(<SingleCard store={store}/>);
+    expect(component.find('#single__card').exists()).toBe(false);
+  });
+  it('Mouse inside initial state should be false', () => {
+    const component = shallow(<SingleCard store={store}/>).dive({ store });
+    expect(component.state('isMouseInside')).toBe(false)
+  });
+  it('Should display and hide delete on mouse enter and leave', () => {
+    const component = shallow(<SingleCard store={store}/>).dive({ store });
+    component.simulate('mouseenter')
+    expect(component.state('isMouseInside')).toBe(true)
+    component.simulate('mouseleave')
+    expect(component.state('isMouseInside')).toBe(false)
+  });
+  it('Should delete card', () => {
+    const copy = store.getState()
+    const component = shallow(<SingleCard store={store}/>).dive({ store })
+    component.simulate('mouseenter')
+    component.find('#single__card__delete').simulate('click');
+    expect(store.getState()).not.toBe(copy)
+    // console.log(component.html())
+  })
+})
